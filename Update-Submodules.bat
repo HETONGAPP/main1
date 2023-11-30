e568a11ba48532ab45d930ce049e3f3198b77038
HETONGAPP <hetongapp@gmail.com>
2023-11-30 10:12:24 -0500

    rr
    r

--- Update-Submodules.bat
+++ Update-Submodules.bat
@@ -17,6 +17,6 @@ for /l %%i in (0, 1, 1) do (
     )
 
     REM Clone the repository
-    git clone --branch !version! https://github.com/HETONGAPP/!name!.git
+    git clone --branch !version! --single-branch https://github.com/HETONGAPP/!name!.git
 )
 endlocal
